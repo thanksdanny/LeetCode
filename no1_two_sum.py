b3ce36879232cfa9ad997519fe923f6c7899b510
hejunjie <hejunjie@yy.com>
2019-05-28 22:45:26 +0800

    重新开始

--- no1_two_sum.py
+++ no1_two_sum.py
@@ -9,58 +9,7 @@ return [0, 1].
 """
 
 
-def two_sum(given_numbs, target):
-    result = []
-    for i in range(len(given_numbs)):
-        for j in range(i + 1, len(given_numbs)):
-            if given_numbs[i] + given_numbs[j] == target:
-                result.append(i)
-                result.append(j)
-                return result
-
-def two_sum2(numbs, target):
-    result = []
-    for i in range(len(numbs)):
-        oneNum = numbs[i]
-        twoNum = target - oneNum
-        if twoNum in numbs:
-            j = numbs.index(twoNum)
-            if i != j:
-                result.append(i)
-                result.append(j)
-                return result
-
-def two_sum3(nums, target):
-    num_dict = {nums[i]: i for i in range(len(nums))}
-    print(num_dict)
-    num_dict2 = {i: target-nums[i] for i in range(len(nums))}
-    print(num_dict2)
-    result = []
-    for i in range(len(nums)):
-        j = num_dict.get(num_dict2.get(i))
-        if (j is not None) and (j != i):
-            result = [i, j]
-            break
-    return result
-
-def two_sum4(nums, target):
-    """
-    目前执行效率最快
-    :param nums:
-    :param target:
-    :return:
-    """
-    n = len(nums)
-    dd = {nums[i]: i for i in range(n)}
-    print(dd)
-    for i in range(n - 1):
-        cha = target - nums[i]
-        if cha in dd and i != dd[cha]:
-            return [i, dd[cha]]
-    return 'null'
-
-
 if __name__ == '__main__':
     nums = [2, 15, 7, 11]
     target = 9
-    print(two_sum4(nums, target))
+    # print(two_sum4(nums, target))
--- no2_add_two_num.py
+++
@@ -1,9 +0,0 @@
-import ListNode
-
-def _addTwoNumbs(l1, l2):
-    """
-    :param l1: ListNode
-    :param l2: ListNode
-    :return: ListNode
-    """
-    p = dummy = ListNode(-1)
