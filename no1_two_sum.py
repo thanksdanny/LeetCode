c846fa67247924c231cc430982c3ef0a8f20ea03
hejunjie <hejunjie@yy.com>
2019-05-28 23:20:12 +0800

    了解到01的较优解

--- no1_two_sum.py
+++ no1_two_sum.py
@@ -6,10 +6,46 @@ such as:
 Given nums = [2, 7, 11, 15], target = 9,
 Because nums[0] + nums[1] = 2 + 7 = 9,
 return [0, 1].
+
+给定一个整数数组 nums 和一个目标值 target，请你在该数组中找出和为目标值的那 两个 整数，并返回他们的数组下标。
+
+你可以假设每种输入只会对应一个答案。但是，你不能重复利用这个数组中同样的元素。
 """
 
 
-if __name__ == '__main__':
-    nums = [2, 15, 7, 11]
-    target = 9
-    # print(two_sum4(nums, target))
+class Solution(object):
+    # def twoSum(self, nums, target):
+    #     """
+    #     :type nums: List[int]
+    #     :type target: int
+    #     :rtype: List[int]
+    #     """
+    #     for a in range(len(nums)):
+    #         for b in range(len(nums)):
+    #             if a != b:
+    #                 result = nums[a] + nums[b]
+    #                 if result == target:
+    #                     print(a, b)
+    #                     return a, b
+
+    # 更好的办法
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        n = len(nums)
+        lookup = {}
+        for i in range(n):
+            tmp = target - nums[i]
+            if tmp in lookup:
+                return [lookup[tmp], i]
+            lookup[nums[i]] = i
+
+if __name__ == "__main__":
+    s = Solution()
+    l = [1, 6, 3, 4]
+    t = 10
+    s.twoSum(l, t)
+
